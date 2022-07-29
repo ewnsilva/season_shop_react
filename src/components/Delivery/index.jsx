741b7dc03b9eaadeef1a336d2adfe26671c3343a
Ericles Willian <83819706+ewnsilva@users.noreply.github.com>
2022-07-28 21:13:11 -0300

    feat: Mensagem de estimativa de entrega do item.

---
+++ src/components/Delivery/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css"
+
+const Delivery = (props) => (
+    <div className="delivery">
+        <h4>Entrega</h4>
+        <p>Receba ainda hoje.</p>
+    </div>
+)
+
+export default Delivery
