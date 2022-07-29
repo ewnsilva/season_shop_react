dbcc25eca68ddd0d24d114615c5d1eb0df93bc66
Ericles Willian <83819706+ewnsilva@users.noreply.github.com>
2022-07-28 21:12:28 -0300

    feat: Input para alterar quantidade de produtos.

---
+++ src/components/Quantity/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css"
+
+const Quantity = (props) => (
+    <div className="quantity">
+        <h4>Quandidade</h4>
+        <input type="number" className="inputNumber"></input>
+    </div>
+)
+
+export default Quantity
