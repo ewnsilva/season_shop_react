bb960978f04f991b01109f193ee826c9e1fa79bf
Ericles Willian <83819706+ewnsilva@users.noreply.github.com>
2022-07-28 21:11:07 -0300

    feat: Botão para voltar a página inicial.

---
+++ src/components/BackButton/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css"
+
+const BackButton = (props) => (
+    <button className="backButton">Adicionar Novos Itens</button>
+)
+
+export default BackButton
