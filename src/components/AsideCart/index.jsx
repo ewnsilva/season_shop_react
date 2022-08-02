efddbaac2ff13526db92b8dcfd9ddf3f0372f52b
Ericles Willian <83819706+ewnsilva@users.noreply.github.com>
2022-08-02 20:08:01 -0300

    feat: Vinculando a funcionalidade de adicionar e remover itens do carrinho.

--- src/components/AsideCart/index.jsx
+++ src/components/AsideCart/index.jsx
@@ -1,16 +1,33 @@
 import React from "react"
-import './style.css';
-import BuyButton from "../BuyButton"
 
-const AsideCart = (props) => (
+import BuyButton from "../BuyButton"
+import useCart from "../../hook/useCart";
+import RemoveButton from "../RemoveButton"
+
+import './style.css';
+
+const AsideCart = (_) => {
+
+    const {cartProducts, removeProduct} = useCart()
+
+    return (
         <aside className="asideCart">
             <div className="asideCart__upperBox">
                 <h2 className="upperBox__title">Carrinho</h2>
-                Teste
+
+                    {cartProducts.map(
+                        product =>
+                        <div key={product.id} className="upperBox__products">
+                            <li>{product.name}</li>
+                            <RemoveButton name="X" handleClick={() => removeProduct(product.id)}/>
+                        </div>
+                    )}
+                
             </div>
             <div className="asideCart__content">
                 <BuyButton name="Comprar"/>
             </div>
         </aside>
-)
+    )
+}
 export default AsideCart
