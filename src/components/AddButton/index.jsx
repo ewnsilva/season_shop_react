16729b7ce506833e608e461d5b2fcf8aa85e2b72
Ericles Willian <83819706+ewnsilva@users.noreply.github.com>
2022-08-02 18:49:10 -0300

    feat: Adicionando funcionalidade ao componente.

--- src/components/AddButton/index.jsx
+++ src/components/AddButton/index.jsx
@@ -1,10 +1,15 @@
+import { click } from "@testing-library/user-event/dist/click";
 import React from "react"
+import AsideCart from "../AsideCart";
 import './style.css';
 
-const AddButton = ({name, product}) => {
+const AddButton = ({name, handleClick}) => {
+        
         return(
-        <button className="addButton">{name}</button>
-        )
+        <button className="addButton" onClick={handleClick}>
+                {name}
+        </button>       
+        )  
 }
 
 export default AddButton
