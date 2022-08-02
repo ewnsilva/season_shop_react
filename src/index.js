6b09cdc045e31bb64e3fdadf7d943cfded1f8b8b
Ericles Willian <83819706+ewnsilva@users.noreply.github.com>
2022-08-02 20:41:27 -0300

    style: Componente envolvido em comentário para testes.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './pages/index/';
+import Index from './pages/index/';
 import Cart from './pages/cart/'
+import {CartProvider} from './context/CartContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Cart />
+    <CartProvider>
+      <Index /> {/* Para ver a página Cart, colocar index em comentario e retirar Cart do comentario.*/}
+      {/* <Cart /> */} {/* Comentado enquanto não foi configurado o mecanismo de rotas.*/}
+    </CartProvider>
   </React.StrictMode>
 );
