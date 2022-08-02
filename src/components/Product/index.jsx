d97068a93642181289c090e7ea78895d045aa2b1
Ericles Willian <83819706+ewnsilva@users.noreply.github.com>
2022-08-02 20:19:09 -0300

    feat: Ajuste de propriedades para implementar funcionalidade de adicionar ao Carrinho.

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,20 +1,28 @@
 import React from "react"
+
 import AddButton from "../AddButton"
-import "./style.css" 
 import box from "../../assets/box.png"
+import useCart from "../../hook/useCart"
 
-const Product = ({listProducts, name, price, description, department, product}) => (
+import "./style.css" 
+
+const Product = ({product}) => {
+
+	const {addToCart} = useCart()
+
+		return(
 		<div className="product">
-			<h5 className="product__name">{name}</h5>
+			<h5 className="product__name">{product.name}</h5>
 			<img className="product__image" src={box} alt="Imagem do Produto" />
-			<p className="product__price">R$ {price}</p>
+			<p className="product__price">R$ {product.price}</p>
 			<div className="extraInfo">
-				<p className="product__description">{description}</p>
-				<p className="product__department">Departamento: {department}</p>
-				<p className="product__product">Tipo do Produto: {product}</p>
+				<p className="product__description">{product.description}</p>
+				<p className="product__department">Departamento: {product.department}</p>
+				<p className="product__product">Tipo do Produto: {product.product}</p>
 			</div>
-			<AddButton name="Adicionar ao Carrinho" product={listProducts} /> 
-
+			<AddButton name="Adicionar ao Carrinho" handleClick={() => addToCart(product)} /> 
 		</div>
-)
+		)
+}
+
 export default Product
