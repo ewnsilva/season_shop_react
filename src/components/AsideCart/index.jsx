7110832e03c9542fd1f505c297004de59c0a8d5b
Ericles Willian <83819706+ewnsilva@users.noreply.github.com>
2022-08-03 18:34:07 -0300

    feat: Vinculando as funcionalidades aos componentes através das propriedades.

--- src/components/AsideCart/index.jsx
+++ src/components/AsideCart/index.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { useNavigate } from "react-router-dom"
 
 import BuyButton from "../BuyButton"
 import useCart from "../../hook/useCart";
@@ -8,24 +9,26 @@ import './style.css';
 
 const AsideCart = (_) => {
 
-    const {cartProducts, removeProduct} = useCart()
+    const { cartProducts, removeProduct } = useCart()
+    const navigate = useNavigate()
+
 
     return (
         <aside className="asideCart">
             <div className="asideCart__upperBox">
                 <h2 className="upperBox__title">Carrinho</h2>
 
-                    {cartProducts.map(
-                        product =>
+                {cartProducts.map(
+                    product =>
                         <div key={product.id} className="upperBox__products">
                             <li>{product.name}</li>
-                            <RemoveButton name="X" handleClick={() => removeProduct(product.id)}/>
+                            <RemoveButton name="X" handleClick={() => removeProduct(product.id)} />
                         </div>
-                    )}
-                
+                )}
+
             </div>
             <div className="asideCart__content">
-                <BuyButton name="Comprar"/>
+                <BuyButton name="Comprar" handleClick={() => { navigate("/cart") }} />
             </div>
         </aside>
     )
