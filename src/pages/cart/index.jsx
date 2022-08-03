76bca6687241a687638a5c6bc6fbd4bdd17b6c52
Ericles Willian <83819706+ewnsilva@users.noreply.github.com>
2022-08-03 18:30:55 -0300

    feat: Adiconando navegação entre páginas e vinculando funcionalidades aos componentes.

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,39 +1,60 @@
 import React from "react";
-import "./style.css"
+import { useNavigate } from "react-router-dom"
 
 import Title from "../../components/Title";
 import SubTitle from "../../components/SubTitle"
 import Identifier from "../../components/Identifier";
-
+import useCart from "../../hook/useCart";
 import BackButton from "../../components/BackButton";
 import BoughtItens from "../../components/BoughtItens"
 import TotalValue from "../../components/TotalValueContainer"
+import Popup from "../../components/Popup";
+import "./style.css"
 
 
-const Cart = () => (
-  <div className="cart">
-    <Title title="Season Shop" />
+const Cart = () => {
+  
+  const {cartProducts, removeProduct} = useCart()
+  const navigate = useNavigate()
 
-    <div className="cart__content">
-      <SubTitle subTitle="Finalizando as Compras" />
-      <Identifier identifier="Seu Carrinho" />
-      <BackButton />
+  return (
+    <div className="cart">
+      <Title title="Season Shop" />
 
-      <div className="content__info">
+      <div className="cart__content">
+        <SubTitle subTitle="Finalizando as Compras" />
+        <Identifier identifier="Seu Carrinho" />
+        <BackButton handleClick={() => { navigate("/") }}/>
 
-        <div className="info__itens">
-          <BoughtItens />
-          <BoughtItens />
-          <BoughtItens />
-        </div>
+        <div className="content__info">
 
-        <div className="info__value">
-          <TotalValue />
+          
+            <div className="info__itens" >
+              {cartProducts.map(
+              product => 
+              
+              <BoughtItens 
+                key={product.id} 
+                infoName={product.name} 
+                infoPrice={product.price}
+                infoRemove={() => removeProduct(product.id)}/>
+            )}
+            </div>
+              
+           
+            
+          
+          
+
+          <div className="info__value">
+            <TotalValue />
+          </div>
+
         </div>
 
       </div>
-
+      <Popup />
     </div>
-  </div>
-)
+  )
+}
 export default Cart
