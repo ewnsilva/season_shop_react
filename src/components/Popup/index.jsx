9edbd411345919c4e123237f0a009d668612731e
Ericles Willian <83819706+ewnsilva@users.noreply.github.com>
2022-08-07 16:37:02 -0300

    feat: Ajuste de modal para correta aparição de popup.

--- src/components/Popup/index.jsx
+++ src/components/Popup/index.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-import Purchase from "../Purchase";
+
 import "./style.css"
 
-const Popup = (_) => {
+const Popup = ({ handleClick }) => {
 
     return (
         <div className="popup__wrapper">
             <div className="popup__window">
                 <div className="popup">
                     <div className="popup__content">
-
                         <h2>Compra finalizada com sucesso!</h2>
-                        <Purchase name="Confirmar"/>
+                        <button className="content__button" onClick={handleClick}>Confirmar</button>
                     </div>
                 </div>
             </div>
         </div>
     )
+
 }
+
 export default Popup
